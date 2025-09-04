29c5fb50b3addb3c2743b61d59d907e21786fb71
Ettore Delprino <edelprino@gmail.com>
2025-09-04 15:28:04 +0200

    Added tracing and tower-http for enhanced logging and tracing in the API server.

--- src/api.rs
+++ src/api.rs
@@ -3,6 +3,7 @@ use axum::{
     Router,
     routing::{get, post},
 };
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 async fn responses() -> &'static str {
     log::info!("Received request at /v1/resposens");
@@ -14,11 +15,18 @@ async fn root() -> &'static str {
 }
 
 pub async fn run() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .try_init()
+        .ok();
+
     let host = std::env::var("API_HOST").unwrap_or("0.0.0.0:8080".to_string());
     log::info!("Starting API server at http://{host}");
     let app = Router::new()
         .route("/", get(root))
-        .route("/v1/resposens", post(responses));
+        .route("/v1/resposens", post(responses))
+        .layer(tower_http::trace::TraceLayer::new_for_http());
     let listener = tokio::net::TcpListener::bind(host).await.unwrap();
     axum::serve(listener, app).await.unwrap();
     Ok(())
